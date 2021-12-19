b9bc5ab46f6ddde50429ea0407aa99bbd517d41f
BoomBunnyBoss <top.evgesha245@gmail.com>
2021-12-19 02:36:17 +0200

    correction of errors in the code

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@ function calcprice() {
     land  = document.getElementById('land').value;
     switch (land) {
        case "agricultural":
-          cena = 1000;
-          break
+            price = 1000;
+            break
        case "dwelling":
-          cena = 1500;
-          break   
+            price = 1500;
+            break   
         case "historica":
-          cena = 2000;
-          break   
+            price = 2000;
+            break   
        default:
-          cena = 1000;
-          break
+            price = 1000;
+            break
     }
-        shirina  = document.getElementById('shirina').value;
-        dlina  = document.getElementById('dlina').value;
+    shirina  = document.getElementById('shirina').value;
+    dlina  = document.getElementById('dlina').value;
     if(shirina == ""){
         alert("Вы не указали ширину");
         } else if(dlina == ""){
@@ -24,7 +24,7 @@ function calcprice() {
     else {
         ploschad = parseFloat (shirina)* parseFloat (dlina);
         document.getElementById('ploschad').innerHTML = "Площадь равна: "+ ploschad +" кв. м.";
-    stoimost = ploschad*cena;
+    stoimost = ploschad*price;
     document.getElementById('stoimost').innerHTML = "Стоимость равна: "+ stoimost +" грн.";
     }
 }
